191c6d7e0a5ffb94cd14b28a0513abd711ce267d
Ousmane Barry <ppobarry@gmail.com>
2022-10-29 03:56:23 -0400

    login and logout

---
+++ app/src/main/java/com/example/coursebooking/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.example.coursebooking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText email, password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            updateUI(mAuth.getCurrentUser());
+        }
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailStr = email.getText().toString();
+                String passStr = password.getText().toString();
+                loginWithEmailPassword(emailStr, passStr);
+            }
+        });
+
+    }
+
+    private void loginWithEmailPassword(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            updateUI(mAuth.getCurrentUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Invalid credentials",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        FirebaseUser user = currentUser;
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra("uuid", user.getUid());
+        startActivity(intent);
+        finish();
+    }
+}

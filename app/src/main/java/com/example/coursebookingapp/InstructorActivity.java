43f11bf4197026b7087f4f72bf765d5ae442121c
Ousmane Barry <ppobarry@gmail.com>
2022-11-13 17:51:44 -0500

    things to do

--- app/src/main/java/com/example/coursebookingapp/InstructorActivity.java
+++ app/src/main/java/com/example/coursebookingapp/InstructorActivity.java
@@ -21,7 +21,7 @@ public class InstructorActivity extends AppCompatActivity implements InstructorR
     Auth auth;
     Store store;
     TextView welcomeTxt;
-    Button logoutBtn;
+    Button logoutBtn, teachBtn;
     ArrayList<Course> courseModels = new ArrayList<>();
 
     @Override
@@ -35,9 +35,15 @@ public class InstructorActivity extends AppCompatActivity implements InstructorR
 
         welcomeTxt = findViewById(R.id.welcomeTxt);
         logoutBtn = findViewById(R.id.logOutBtn);
+        teachBtn = findViewById(R.id.teachCourse);
 
         store.getUserDocument(uuid).addOnSuccessListener(documentSnapshot -> {
             welcomeTxt.setText(String.format("Welcome, %s! (%s)", documentSnapshot.get("name"), documentSnapshot.get("accountType")));
+        });
+
+        teachBtn.setOnClickListener(view -> {
+            // logic to assign a course to instructor by searching all the available courses that
+            // are not already assigned to an instructor
         });
 
         logoutBtn.setOnClickListener(view -> {
@@ -51,17 +57,20 @@ public class InstructorActivity extends AppCompatActivity implements InstructorR
 
     @Override
     public void onViewClick(int position) {
-
+        // logic to show the course information
+        // day, hours, course code, course name, course description
     }
 
     @Override
     public void onEditClick(int position) {
-
+        // logic to edit the course information
+        // day, hours, course description, capacity
     }
 
     @Override
     public void onDeleteClick(int position) {
-
+        // logic to unassign a course from the currently signed in instructor
+        // remove day, hours, hasInstructor == false, description, capacity, instructorId
     }
 
     private void loadCourses() {

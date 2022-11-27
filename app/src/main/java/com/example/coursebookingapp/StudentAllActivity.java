85317ae302c49b61a1de69abe2ce42a74f57b370
Hasbiya Y <has.y.spring@gmail.com>
2022-11-27 14:30:22 -0500

    Added base layout for student activity xmls

---
+++ app/src/main/java/com/example/coursebookingapp/StudentAllActivity.java
@@ -0,0 +1,14 @@
+package com.example.coursebookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_all);
+    }
+}

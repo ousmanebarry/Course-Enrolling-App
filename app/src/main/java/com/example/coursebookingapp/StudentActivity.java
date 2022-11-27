5ac64dd0bade0dfd000f9f4034a806c71640e3f5
Hasbiya Y <has.y.spring@gmail.com>
2022-11-27 16:22:39 -0500

    Bulk of Student Activity code

--- app/src/main/java/com/example/coursebookingapp/StudentActivity.java
+++ app/src/main/java/com/example/coursebookingapp/StudentActivity.java
@@ -1,24 +1,170 @@
 package com.example.coursebookingapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.coursebookingapp.classes.Course;
+import com.example.coursebookingapp.database.Auth;
+import com.example.coursebookingapp.database.Store;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class StudentActivity extends AppCompatActivity implements StudentRecyclerViewInterface {
 
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+    Auth auth;
+    Store store;
+    TextView deleteText, deleteYesBtn, deleteCancelBtn;
+    TextView viewCourseName, viewCourseCode, viewCourseDays, viewCourseHours, viewCourseCapacity, viewCourseDesc, viewCancel;
+    Button logoutBtn, enrollBtn;
+    ArrayList<Course> courseModels = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        auth = new Auth();
+        store = new Store();
+        String uuid = auth.getCurrentUser().getUid();
+
+        logoutBtn = findViewById(R.id.logOutBtn);
+        enrollBtn = findViewById(R.id.enrollCourse);
+        ActivityResultLauncher<Intent> studentCourseActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::onActivityResult);
+
+        store.getUserDocument(uuid).addOnSuccessListener(documentSnapshot -> {
+            String welcome = String.format("Welcome, %s! (%s)", documentSnapshot.get("name"), documentSnapshot.get("accountType"));
+            Objects.requireNonNull(getSupportActionBar()).setTitle(welcome);
+        });
+
+        enrollBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), StudentActivity.class);
+            studentCourseActivityResultLauncher.launch(intent);
+        });
+
+        logoutBtn.setOnClickListener(view -> {
+            auth.signOut();
+            updateScreen();
+            finish();
+        });
+
+        loadCourses();
     }
 
     @Override
     public void onViewClick(int position) {
 
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View viewCoursePopupView = getLayoutInflater().inflate(R.layout.student_view_course_popup, null);
+        String id = courseModels.get(position).getDocID();
+
+        viewCourseName = viewCoursePopupView.findViewById(R.id.courseName) ;
+        viewCourseCode = viewCoursePopupView.findViewById(R.id.courseCode);
+        viewCourseDays = viewCoursePopupView.findViewById(R.id.courseDays);
+        viewCourseHours = viewCoursePopupView.findViewById(R.id.courseHours);
+        viewCourseCapacity = viewCoursePopupView.findViewById(R.id.courseCapacity);
+        viewCourseDesc = viewCoursePopupView.findViewById(R.id.courseDesc);
+        viewCancel = viewCoursePopupView.findViewById(R.id.viewCancel);
+
+        store.getCourseDocument(id).addOnSuccessListener(snapshot -> {
+            viewCourseName.setText(Objects.requireNonNull(snapshot.get("name")).toString());
+            viewCourseCode.setText(Objects.requireNonNull(snapshot.get("code")).toString());
+            viewCourseDays.setText(Objects.requireNonNull(snapshot.get("days")).toString());
+            viewCourseHours.setText(Objects.requireNonNull(snapshot.get("hours")).toString());
+            viewCourseCapacity.setText(Objects.requireNonNull(snapshot.get("capacity")).toString());
+            viewCourseDesc.setText(Objects.requireNonNull(snapshot.get("description")).toString());
+        });
+
+
+        dialogBuilder.setView(viewCoursePopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        viewCancel.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
     }
 
     @Override
-    public void onDeleteClick(int position) {
+    public void onDeleteClick(int position){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View deleteCoursePopupView = getLayoutInflater().inflate(R.layout.instructor_delete_course_popup, null);
+        String id = courseModels.get(position).getDocID();
+
+        deleteText = deleteCoursePopupView.findViewById(R.id.deleteText);
+        deleteYesBtn = deleteCoursePopupView.findViewById(R.id.yesBtn);
+        deleteCancelBtn = deleteCoursePopupView.findViewById(R.id.cancelBtn);
+
+        deleteText.setText(getString(R.string.course_unenroll_confirmation_student, courseModels.get(position).getCode()));
+
+        dialogBuilder.setView(deleteCoursePopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        deleteYesBtn.setOnClickListener(view -> {
+            String name = courseModels.get(position).getName();
+            String code = courseModels.get(position).getCode();
+
+            dialog.dismiss();
+            loadCourses();
+        });
+
+        deleteCancelBtn.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+
+        loadCourses();
+    }
+
+    public void onActivityResult(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            loadCourses();
+        }
+    }
+
+    private void loadCourses() {
+        RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
+        courseModels = new ArrayList<>();
+
+        store.getStudentCourses(auth.getCurrentUser().getUid()).addOnSuccessListener(query -> {
+            for (DocumentSnapshot snapshot : query) {
+                String docID = Objects.requireNonNull(snapshot.getId());
+                String name = Objects.requireNonNull(snapshot.get("name")).toString();
+                String code = Objects.requireNonNull(snapshot.get("code")).toString();
+                String capacity = Objects.requireNonNull(snapshot.get("capacity")).toString();
+
+                Course studentCourseModel = new Course(name, code, docID, capacity);
+                courseModels.add(studentCourseModel);
+            }
+
+            StudentCourseRecyclerViewAdapter adapter = new InstructorCourseRecyclerViewAdapter(this, courseModels, StudentActivity.this);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        });
+
 
     }
+
+    private void updateScreen() {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/coursebookingapp/StudentAllActivity.java
+++ app/src/main/java/com/example/coursebookingapp/StudentAllActivity.java
@@ -1,31 +1,171 @@
 package com.example.coursebookingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-<<<<<<< HEAD
+import com.example.coursebookingapp.classes.Course;
+import com.example.coursebookingapp.database.Auth;
+import com.example.coursebookingapp.database.Store;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class StudentAllActivity extends AppCompatActivity {
-=======
-public class StudentAllActivity extends AppCompatActivity implements StudentAllRecyclerViewInterface {
->>>>>>> f1cedc217f65d01206e7467726faf2f503c28497
+    Button backBtn, viewAllBtn;
+    ArrayList<Course> courseModels;
+    SearchView searchCourses;
+    Store store;
+    Auth auth;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private RecyclerView recyclerView;
+
+    TextView studentPick, studentCancel,iewCourseName, viewCourseCode, viewCourseName,
+            viewCourseDays, viewCourseHours, viewCourseCapacity, viewCourseDesc, viewCancel, viewInstructorName;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_all);
+
+        store = new Store();
+        auth = new Auth();
+        recyclerView = findViewById(R.id.mRecyclerView);
+        backBtn = findViewById(R.id.backBtn);
+        viewAllBtn = findViewById((R.id.viewAllBtn));
+        searchCourses = findViewById(R.id.searchCourses);
+        searchCourses.clearFocus();
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("All Courses");
+
+        searchCourses.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadCourses(query);
+                Objects.requireNonNull(getSupportActionBar()).setTitle("Custom Courses");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        viewAllBtn.setOnClickListener(view -> {
+            loadCourses();
+            Objects.requireNonNull(getSupportActionBar()).setTitle("All Courses");
+        });
+
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        loadCourses();
+
     }
-<<<<<<< HEAD
-=======
 
     @Override
-    public void onViewClick(int position) {
+    public void onViewClick(int position){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View viewCoursePopupView = getLayoutInflater().inflate(R.layout.student_all_view_course_popup, null);
+        String id = courseModels.get(position).getDocID();
+        String noData = "Unassigned";
 
+        viewCourseName = viewCoursePopupView.findViewById(R.id.courseName);
+        viewCourseCode = viewCoursePopupView.findViewById(R.id.courseCode);
+        viewInstructorName = viewCoursePopupView.findViewById(R.id.instructorName);
+        viewCourseDays = viewCoursePopupView.findViewById(R.id.courseDays);
+        viewCourseHours = viewCoursePopupView.findViewById(R.id.courseHours);
+        viewCourseCapacity = viewCoursePopupView.findViewById(R.id.courseCapacity);
+        viewCourseDesc = viewCoursePopupView.findViewById(R.id.courseDesc);
+        viewCancel = viewCoursePopupView.findViewById(R.id.viewCancel);
+
+        store.getCourseDocument(id).addOnSuccessListener(snapshot -> {
+            viewCourseName.setText(snapshot.get("name") == null ? noData : snapshot.get("name").toString());
+            viewCourseCode.setText(snapshot.get("code") == null ? noData : snapshot.get("code").toString());
+            viewCourseDays.setText(snapshot.get("days") == null ? noData : snapshot.get("days").toString());
+            viewCourseHours.setText(snapshot.get("hours") == null ? noData : snapshot.get("hours").toString());
+            viewCourseCapacity.setText(snapshot.get("capacity") == null ? noData : snapshot.get("capacity").toString());
+            viewCourseDesc.setText(snapshot.get("description") == null ? noData : snapshot.get("description").toString());
+
+            Boolean hasInstructor = snapshot.get("hasInstructor", Boolean.class);
+
+            if (Boolean.TRUE.equals(hasInstructor)) {
+                store.getUserDocument(snapshot.get("instructorId").toString()).addOnSuccessListener(snapshotTwo -> {
+                    viewInstructorName.setText(snapshotTwo.get("name").toString());
+                });
+            } else {
+                viewInstructorName.setText(noData);
+
+            }
+        });
+
+        dialogBuilder.setView(viewCoursePopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        viewCancel.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
     }
 
-    @Override
-    public void onEnrollClick(int position) {
+    private void loadCourses() {
+        courseModels = new ArrayList<>();
 
+        store.getAllCourses().addOnSuccessListener(query -> {
+            for (DocumentSnapshot snapshot : query) {
+                String docID = Objects.requireNonNull(snapshot.getId());
+                String name = Objects.requireNonNull(snapshot.get("name")).toString();
+                String code = Objects.requireNonNull(snapshot.get("code")).toString();
+
+                Course instructorCourseModel = new Course(name, code, docID);
+                courseModels.add(instructorCourseModel);
+            }
+
+            InstructorAllCourseRecyclerViewAdapter adapter = new StudentAllCourseRecyclerViewAdapter(this, courseModels, StudentAllActivity.this);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        });
     }
->>>>>>> f1cedc217f65d01206e7467726faf2f503c28497
+
+    //Missing OnTeachClick Section
+
+
+    private void loadCourses(String courseNameCode) {
+        courseModels = new ArrayList<>();
+
+        store.getCoursesByNameOrCode(courseNameCode).addOnSuccessListener(querySnapshots -> {
+            for (QuerySnapshot queryDocumentSnapshots : querySnapshots) {
+                for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                    String docID = Objects.requireNonNull(snapshot.getId());
+                    String name = Objects.requireNonNull(snapshot.get("name")).toString();
+                    String code = Objects.requireNonNull(snapshot.get("code")).toString();
+
+                    Course instructorCourseModel = new Course(name, code, docID);
+
+                    courseModels.add(instructorCourseModel);
+                }
+            }
+
+
+            InstructorAllCourseRecyclerViewAdapter adapter = new InstructorAllCourseRecyclerViewAdapter(this, courseModels, InstructorAllActivity.this);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        });
+    }
 }

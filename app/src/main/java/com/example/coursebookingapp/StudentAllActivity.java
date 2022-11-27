a843f23ecea600b526406486e4903f9292c9f8b3
Hasbiya Y <has.y.spring@gmail.com>
2022-11-27 18:36:58 -0500

    Some other changes across the files for the popups

--- app/src/main/java/com/example/coursebookingapp/StudentAllActivity.java
+++ app/src/main/java/com/example/coursebookingapp/StudentAllActivity.java
@@ -11,10 +11,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.coursebookingapp.classes.Course;
 import com.example.coursebookingapp.database.Auth;
 import com.example.coursebookingapp.database.Store;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -33,7 +35,9 @@ public class StudentAllActivity extends AppCompatActivity implements StudentAllR
     private AlertDialog dialog;
     private RecyclerView recyclerView;
 
-    TextView studentPick, studentCancel,viewCourseName, viewCourseCode,
+    private final String CAPACITY_NUM = "0";
+
+    TextView studentPickBtn, studentCancelBtn,viewCourseName, viewCourseCode,
             viewCourseDays, viewCourseHours, viewCourseCapacity, viewCourseDesc, viewCancel, viewInstructorName;
 
     @Override
@@ -127,6 +131,34 @@ public class StudentAllActivity extends AppCompatActivity implements StudentAllR
 
     @Override
     public void onEnrollClick(int position){
+        store.getCourseDocument(courseModels.get(position).getDocID()).addOnSuccessListener(snapshot -> {
+
+            dialogBuilder = new AlertDialog.Builder(this);
+            final View pickCoursePopupView = getLayoutInflater().inflate(R.layout.student_enroll_course_popup, null);
+
+            viewCourseName = pickCoursePopupView.findViewById(R.id.courseName);
+            viewCourseCode = pickCoursePopupView.findViewById(R.id.courseCode);
+            viewInstructorName = pickCoursePopupView.findViewById(R.id.courseInstructor);
+            viewCourseDays = pickCoursePopupView.findViewById(R.id.courseDays);
+            viewCourseHours = pickCoursePopupView.findViewById(R.id.courseHours);
+            viewCourseCapacity = pickCoursePopupView.findViewById(R.id.courseCapacity);
+            viewCourseDesc = pickCoursePopupView.findViewById(R.id.courseDesc);
+            studentCancelBtn = pickCoursePopupView.findViewById(R.id.viewCancel);
+            studentPickBtn = pickCoursePopupView.findViewById(R.id.enrollBtn);
+
+            dialogBuilder.setView(pickCoursePopupView);
+            dialog = dialogBuilder.create();
+            dialog.show();
+
+            studentPickBtn.setOnClickListener(v -> {
+
+                String capacity = viewCourseCapacity.toString();
+                if(capacity.matches(CAPACITY_NUM)) {
+                    Toast.makeText(StudentAllActivity.this, "Enrollment failed. Course Capacity is full at this time.", Toast.LENGTH_SHORT).show();
+                };
+            });
+
+        });
 
     }
 
@@ -148,9 +180,6 @@ public class StudentAllActivity extends AppCompatActivity implements StudentAllR
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         });
     }
-
-    //Missing OnTeachClick Section
-
 
     private void loadCourses(String courseNameCode) {
         courseModels = new ArrayList<>();
